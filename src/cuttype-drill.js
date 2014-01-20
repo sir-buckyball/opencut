3386eb2c22663f29138f8b6f11d5f76213408d4b
Buck Clay <sir-buckyball@users.noreply.github.com>
2014-01-19 22:12:06 -0500

    rewriting drill code. validation up front, pecking, rapid movements where safe.

--- src/cuttype-drill.js
+++ src/cuttype-drill.js
@@ -8,34 +8,45 @@ window.opencut.registerCutType("drill", function generatePathCut(workspace, cut)
   var warnings = [];
   var gcode = [];
 
+  // Basic validations.
   if (cut.points === undefined || cut.points === null || cut.points.length === 0) {
-    throw "path cut does not have any 'points'";
+    throw "drill cut does not have any 'points'";
   }
-
   if (cut.depth === undefined || typeof cut.depth != "number" || cut.depth >= 0) {
-    throw "path cut has an invalid 'depth'";
+    throw "drill cut has an invalid 'depth'";
+  }
+  for (var j = 0; j < cut.points.length; j++) {
+    if (cut.points[j].length != 2) {
+      throw "drill cut expects [x,y] points: " + JSON.stringify(cut.points[j]);
+    }
+    if (typeof cut.points[j][0] != "number" ||
+        typeof cut.points[j][1] != "number") {
+      throw "drill cut expects numeric points: " + JSON.stringify(cut.points[j]);
+    }
   }
 
+
+  // up, over, down, repeat.
   gcode.push("G90");
   for (var i = 0; i < cut.points.length; i++) {
-    if (cut.points[i].length != 2) {
-      warnings.push("invalid 2D point: " + JSON.stringify(cut.points[i]));
-      gcode = [];
-      break;
-    }
+    gcode.push("G0 Z" + workspace.safety_height + " F" + workspace.plunge_rate);
+    gcode.push("G0" +
+        " X" + cut.points[i][0] +
+        " Y" + cut.points[i][1] +
+        " F" + workspace.feed_rate);
 
-    var x = cut.points[i][0];
-    var y = cut.points[i][1];
-    if (typeof x != "number" || typeof y != "number") {
-      warnings.push("invalid numeric values: " + JSON.stringify(cut.points[i]));
-      gcode = [];
-      break;
+    // Perform peck drilling.
+    var z = Math.max(0, cut.depth);
+    var numZPasses = Math.ceil(-cut.depth / workspace.z_step_size);
+    for (var k = 0; k < numZPasses; k++) {
+      if (z > 0) {
+        z = Math.max(cut.depth, -workspace.z_step_size);
+      } else {
+        z = Math.max(cut.depth, z - workspace.z_step_size);
+      }
+      gcode.push("G1 Z" + z + " F" + workspace.plunge_rate);
+      gcode.push("G1 Z0");
     }
-
-    // up, over, down, repeat.
-    gcode.push("G1 Z" + workspace.safety_height + " F" + workspace.plunge_rate);
-    gcode.push("G0 X" + x + " Y" + y + " F" + workspace.feed_rate);
-    gcode.push("G1 Z" + cut.depth + " F" + workspace.plunge_rate);
   }
 
   // go back to the safety height at a rate consistent with out other holes.
