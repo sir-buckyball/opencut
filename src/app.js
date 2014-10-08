442599c7170f689923167d472c37a894052c500e
Buck Clay <clayb.rpi@gmail.com>
2014-10-07 22:34:37 -0700

    adding a first pass at a chrome app. open, save, export, preview are implemented. still need to fix up the UI, add a build script, add help, ...

---
+++ src/app.js
@@ -0,0 +1,155 @@
+var app = angular.module('opencutApp', ['ui.codemirror']);
+app.controller('Ctrl', function ($scope) {
+  var renderer = new opencutPaper('preview-area');
+
+  $scope.showEditor = true;
+  $scope.showGcode = false;
+  $scope.showPreview = true;
+
+  $scope.yamlEditorOptions = {
+        lineWrapping : true,
+        lineNumbers: true,
+        mode: 'yaml',
+    };
+  $scope.gcodeEditorOptions = {mode: 'text', readonly: 'nocursor'};
+
+  $scope.yamlErrors = [];
+  $scope.gcode = [];
+
+  $scope.$watch('jobYaml', function(newValue, oldValue) {
+    if (!$scope.job || !angular.equals(newValue, oldValue)) {
+      $scope.yamlErrors = [];
+      try {
+        $scope.job = YAML.parse(newValue);
+      } catch (err) {
+        $scope.yamlErrors = [err.message];
+      }
+    }
+  });
+
+  $scope.$watch('job', function(newValue, oldValue) {
+    if (!$scope.compiledJob || !angular.equals(newValue, oldValue)) {
+      renderer.renderJob(newValue);
+      if (newValue) {
+        $scope.compiledJob = window.opencut.toGCode(newValue);
+        $scope.gcode = $scope.compiledJob.gcode;
+      } else {
+        $scope.compiledJob = {};
+        $scope.gcode = [];
+      }
+    }
+  });
+
+
+  $scope.jobYaml = YAML.stringify({
+    "name": "test_job",
+    "units": "inch",
+    "bit_diameter": 0.25,
+    "feed_rate": 10,
+    "plunge_rate": 5,
+    "safety_height": 0,
+    "z_step_size": 0.1,
+    "cuts": [{
+      "type": "profile",
+      "depth": -0.125,
+      "side": "inside",
+      "shape": {
+        "type": "rectangle",
+        "origin": [1, 1],
+        "size": [0.5, 0.75]
+      }
+    }]
+  }, 4, 2);
+
+  $scope.toPrettyJson = function(obj) {
+    return JSON.stringify(obj, null, '  ');
+  };
+
+  $scope.toYaml = function(obj) {
+    return YAML.stringify(obj, 3, 2);
+  };
+
+
+  $scope.yamlFile = null;
+
+  $scope.openFile = function() {
+    chrome.fileSystem.chooseEntry({
+      'type': 'openFile',
+      'accepts': [{
+        'description': 'YAML files',
+        'extensions': ['yml', 'yaml', '.txt']
+      }]
+    }, function(entry) {
+      $scope.$apply(function() {
+        $scope.yamlFile = entry;
+        $scope.job = null
+      });
+      entry.file(function(file) {
+        var reader = new FileReader();
+        reader.onerror = console.error;
+        reader.onabort = console.error;
+        reader.onloadend = function(e) {
+          console.log("I did done load");
+          $scope.$apply(function() {
+            $scope.jobYaml = e.target.result;
+          });
+        };
+        reader.readAsText(file);
+      });
+    });
+  };
+
+  var writeToFileFn = function(fileContents, cleanupFn) {
+    return function(entry) {
+      entry.createWriter(function(writer) {
+        writer.onerror = console.error;
+        writer.onwriteend = function() {
+          console.log("done saving file: " + entry.fullPath);
+          if (cleanupFn) {
+            cleanupFn(entry);
+          }
+        };
+        writer.write(new Blob([fileContents], {'type': 'text/x-yaml'}));
+      }, console.error);
+    }
+  };
+
+  $scope.saveNewFile = function() {
+    var suggestedName = "untitled.yaml";
+    chrome.fileSystem.chooseEntry({
+      'type': 'saveFile',
+      'suggestedName': suggestedName
+    }, writeToFileFn($scope.jobYaml, function(entry) {
+      $scope.$apply(function() {
+        $scope.yamlFile = entry;
+      });
+    }));
+  };
+
+  $scope.saveFile = function() {
+    if (!$scope.yamlFile) {
+      $scope.saveNewFile();
+      return;
+    } else {
+      chrome.fileSystem.getWritableEntry($scope.yamlFile,
+          writeToFileFn($scope.jobYaml));
+    }
+  };
+
+  $scope.gcodeFile = null;
+  $scope.exportGcode = function() {
+    var suggestedName = "untitled.gcode";
+    if ($scope.gcodeFile && $scope.gcodeFile.name) {
+      suggestedName = $scope.gcodeFile.name;
+    }
+    // TODO: suggest a name based off the yaml file.
+    chrome.fileSystem.chooseEntry({
+      'type': 'saveFile',
+      'suggestedName': suggestedName
+    }, writeToFileFn($scope.gcode.join('\n'), function(entry) {
+      $scope.$apply(function() {
+        $scope.gcodeFile = entry;
+      });
+    }));
+  };
+});
